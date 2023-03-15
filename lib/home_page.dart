ba78dae1bb9469496535421d91cb30b056912fdd
Your Name <chamidilshan55@gmail.com>
2023-03-15 11:13:53 +0530

    started project

---
+++ lib/home_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: [
+          ElevatedButton(onPressed: () {}, child: Text('Create')),
+          ElevatedButton(onPressed: () {}, child: Text('Read')),
+          ElevatedButton(onPressed: () {}, child: Text('Update')),
+          ElevatedButton(onPressed: () {}, child: Text('Delete'))
+        ],
+      ),
+    );
+  }
+}
